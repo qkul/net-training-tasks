0f0d32d3294f794c339420e030081a3ee291b1f7
q_kul <q_kul@mail.ru>
2019-01-03 15:57:02 +0300

    reflection

---
+++ 10-Reflection/Reflection.Tasks/CodeGeneration.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Reflection.Tasks
+{
+    public class CodeGeneration
+    {
+        /// <summary>
+        /// Returns the functions that returns vectors' scalar product:
+        /// (a1, a2,...,aN) * (b1, b2, ..., bN) = a1*b1 + a2*b2 + ... + aN*bN
+        /// Generally, CLR does not allow to implement such a method via generics to have one function for various number types:
+        /// int, long, float. double.
+        /// But it is possible to generate the method in the run time! 
+        /// See the idea of code generation using Expression Tree at: 
+        /// http://blogs.msdn.com/b/csharpfaq/archive/2009/09/14/generating-dynamic-methods-with-expression-trees-in-visual-studio-2010.aspx
+        /// </summary>
+        /// <typeparam name="T">number type (int, long, float etc)</typeparam>
+        /// <returns>
+        ///   The function that return scalar product of two vectors
+        ///   The generated dynamic method should be equal to static MultuplyVectors (see below).   
+        /// </returns>
+        public static Func<T[], T[], T> GetVectorMultiplyFunction<T>() where T : struct {
+            // TODO : Implement GetVectorMultiplyFunction<T>.
+            // throw new NotImplementedException();
+            var first = Expression.Parameter(typeof(T[]), "first");
+            var second = Expression.Parameter(typeof(T[]), "second");
+            var result = Expression.Parameter(typeof(T), "result");
+            var index = Expression.Parameter(typeof(int), "index");
+            var label = Expression.Label();
+
+            var multAddIncrement = Expression.Block(Expression.AddAssign(result,
+                                    Expression.Multiply(Expression.ArrayIndex(first, index),
+                                                        Expression.ArrayIndex(second, index))),
+                            Expression.PostIncrementAssign(index));
+
+            var block = Expression.Block(
+                new[] { result, index },
+                 Expression.Loop(
+                    Expression.IfThenElse(
+                        Expression.LessThan(index, 
+                            Expression.ArrayLength(first)),
+                             multAddIncrement,
+                                Expression.Break(label, result)),
+                                    label),
+                                    result);
+
+            return Expression.Lambda<Func<T[], T[], T>>(block, first, second).Compile();
+        }
+        // Static solution to check performance benchmarks
+        public static int MultuplyVectors(int[] first, int[] second)
+        {
+            int result = 0;
+            for (int i = 0; i < first.Length; i++)
+            {
+                result += first[i] * second[i];
+            }
+            return result;
+        }
+    }
+}
---
+++ 10-Reflection/Reflection.Tasks/CommonTasks.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Reflection.Tasks
+{
+    public static class CommonTasks
+    {
+
+        /// <summary>
+        /// Returns the lists of public and obsolete classes for specified assembly.
+        /// Please take attention: classes (not interfaces, not structs)
+        /// </summary>
+        /// <param name="assemblyName">name of assembly</param>
+        /// <returns>List of public but obsolete classes</returns>
+        public static IEnumerable<string> GetPublicObsoleteClasses(string assemblyName) {
+            // TODO : Implement GetPublicObsoleteClasses method
+            // throw new NotImplementedException();
+            var assembly = Assembly.ReflectionOnlyLoad(assemblyName);
+            return assembly.GetExportedTypes().
+                Where(x => x.IsClass && x.GetCustomAttributes(typeof(ObsoleteAttribute), true).Any()).
+                Select(x => x.Name);
+
+        }
+
+        /// <summary>
+        /// Returns the value for required property path
+        /// </summary>
+        /// <example>
+        ///  1) 
+        ///  string value = instance.GetPropertyValue("Property1")
+        ///  The result should be equal to invoking statically
+        ///  string value = instance.Property1;
+        ///  2) 
+        ///  string name = instance.GetPropertyValue("Property1.Property2.FirstName")
+        ///  The result should be equal to invoking statically
+        ///  string name = instance.Property1.Property2.FirstName;
+        /// </example>
+        /// <typeparam name="T">property type</typeparam>
+        /// <param name="obj">source object to get property from</param>
+        /// <param name="propertyPath">dot-separated property path</param>
+        /// <returns>property value of obj for required propertyPath</returns>
+        public static T GetPropertyValue<T>(this object obj, string propertyPath) {
+            // TODO : Implement GetPropertyValue method
+            //throw new NotImplementedException();
+            var properties = propertyPath.Split('.');
+            foreach (var prop in properties)
+            {
+                obj = obj.GetType().GetProperty(prop).GetValue(obj, null);
+            }
+            return (T)obj;
+        }
+
+
+        /// <summary>
+        /// Assign the value to the required property path
+        /// </summary>
+        /// <example>
+        ///  1)
+        ///  instance.SetPropertyValue("Property1", value);
+        ///  The result should be equal to invoking statically
+        ///  instance.Property1 = value;
+        ///  2)
+        ///  instance.SetPropertyValue("Property1.Property2.FirstName", value);
+        ///  The result should be equal to invoking statically
+        ///  instance.Property1.Property2.FirstName = value;
+        /// </example>
+        /// <param name="obj">source object to set property to</param>
+        /// <param name="propertyPath">dot-separated property path</param>
+        /// <param name="value">assigned value</param>
+        public static void SetPropertyValue(this object obj, string propertyPath, object value) {
+            // TODO : Implement SetPropertyValue method
+            var properties = propertyPath.Split('.');
+            var newObj = properties.Length == 1 ? obj :
+                GetPropertyValue<object>(obj, String.Join(".", properties.Take(properties.Length - 1)));
+            newObj.GetType().
+                BaseType.InvokeMember(properties.Last(), BindingFlags.FlattenHierarchy | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.SetProperty, null, newObj, new object[] { value });
+
+        }
+    }
+}
